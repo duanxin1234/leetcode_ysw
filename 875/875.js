bfb11ad981b6354426070913498dbd783099d474
duanxin1234 <863351709@qq.com>
2020-07-31 17:15:40 +0800

    星星吃香蕉

---
+++ 875/875.js
@@ -0,0 +1,9 @@
+// @AudioParam  {Number[]} piles
+
+
+var minEatingSpeed = function (piles, H) {
+    let lo = 1;
+    hi = Math.max(...piles);
+    console.log(lo,hi);
+}
+minEatingSpeed([3,6,7,11],8)
---
+++ 875/readme.md
@@ -0,0 +1,2 @@
+[875. 爱吃香蕉的珂珂](https://leetcode-cn.com/problems/koko-eating-bananas/)
+[azl](https://github.com/azl397985856/leetcode/blob/master/problems/875.koko-eating-bananas.md)
